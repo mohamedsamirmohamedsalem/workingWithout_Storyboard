5e354e8f02aac08129773cea53a485ff75d57ba3
Mohamed Samir <msalem@egypt.itgsolutions.com>
2020-09-07 11:37:01 +0200

    Initial Commit

---
+++ workingWithout_Storyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  workingWithout_Storyboard
+//
+//  Created by Mohamed Samir on 9/7/20.
+//  Copyright © 2020 Mohamed Samir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
